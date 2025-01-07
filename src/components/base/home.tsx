b50fef34c6868ed5a0c5c61421e0cf4f12010fe7
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-07 16:47:48 +0900

    ファイルの作成と更新

---
+++ src/components/base/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
