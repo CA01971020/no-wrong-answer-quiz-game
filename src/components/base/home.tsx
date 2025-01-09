958999401bec191d705ced33b16e9dae5d432a19
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-09 14:09:34 +0900

    更新

--- src/components/base/home.tsx
+++ src/components/base/home.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 
 function Home() {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
-  );
+  const [start, setStart] = useState<boolean>(false);
+  useEffect(() => {}, [start]);
+  const clickStartButton = () => {
+    setStart(true);
+  };
+  if (start === true) {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
+  } else
+    return (
+      <div className="relative">
+        <h2 className="absolute flex w-full justify-center top-40">
+          No Wrong Answer Quiz
+        </h2>
+        <button
+          className=" absolute flex w-full justify-center top-60"
+          onClick={clickStartButton}
+        >
+          start
+        </button>
+      </div>
+    );
 }
 
 export default Home;
