82fef28067355f345c9915b65eef849ed889683b
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-09 14:31:09 +0900

    headerの更新

--- src/components/base/header.tsx
+++ src/components/base/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <h2>No Wrong Answer Quiz</h2>
+      <h2 className="absolute left-4 top-4">No Wrong Answer Quiz</h2>
     </div>
   );
 }
